8fa0e0153a57061f9e18ff3258f235bc2f089d9f
yehudah <yehudah.asher@gmail.com>
2015-06-18 15:15:14 +0300

    Solution Initialize

---
+++ WpfPracticesLib/Windows/UIHelper.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfPracticesLib.Windows
+{
+    public static class UIHelper
+    {
+        public static T FindAncestor<T>(DependencyObject dependencyObject)
+            where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(dependencyObject);
+            if (parent == null) return null;
+
+            var parentT = parent as T;
+            return parentT ?? FindAncestor<T>(parent);
+        }
+
+        public static T FindAncestorInScope<T>(DependencyObject dependencyObject, DependencyObject scope)
+            where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(dependencyObject);
+
+            if (parent == null) return null;
+            if (parent == scope) return null;
+
+            var parentT = parent as T;
+            return parentT ?? FindAncestorInScope<T>(parent, scope);
+        }
+
+        /// <summary>
+        /// Finds a Child of a given item in the visual tree. 
+        /// </summary>
+        /// <param name="parent">A direct parent of the queried item.</param>
+        /// <typeparam name="T">The type of the queried item.</typeparam>
+        /// <param name="childName">x:Name or Name of child. </param>
+        /// <returns>The first parent item that matches the submitted type parameter. 
+        /// If not matching item can be found, 
+        /// a null parent is being returned.</returns>
+        public static T FindChild<T>(DependencyObject parent, string childName)
+           where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        /// <summary>
+        /// Looks for a child control within a parent by type
+        /// </summary>
+        public static T FindChild<T>(this DependencyObject parent)
+            where T : DependencyObject
+        {
+            // confirm parent is valid.
+            if (parent == null) return null;
+            if (parent is T) return parent as T;
+
+            DependencyObject foundChild = null;
+
+            if (parent is FrameworkElement) (parent as FrameworkElement).ApplyTemplate();
+
+            if (parent is TextBlock)
+            {
+                return FindInline<T>(parent as TextBlock);
+            }
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                foundChild = FindChild<T>(child);
+                if (foundChild != null) break;
+            }
+
+            return foundChild as T;
+        }
+
+
+        public static T FindInline<T>(this TextBlock parent)
+            where T : DependencyObject
+        {
+            foreach (var inline in parent.Inlines)
+            {
+                T element = FindChild<T>(inline);
+                if (element != null) return element;
+            }
+
+            return null;
+        }
+
+
+        public static void ApplyBinding(DependencyObject target, DependencyProperty property, BindingBase binding)
+        {
+            if (binding != null)
+            {
+                BindingOperations.SetBinding(target, property, binding);
+            }
+            else
+            {
+                BindingOperations.ClearBinding(target, property);
+            }
+        }
+    }
+}
